b9069f3167fc81ce5c6fbf630d48abb2bf658b32
Erica Rottman <erica7650@gmail.com>
2017-07-10 11:34:12 -0700

    working get call for tenents

--- src/components/TenentsPage.jsx
+++ src/components/TenentsPage.jsx
@@ -3,9 +3,31 @@ import { Link } from 'react-router'
 import { Accordion, ListGroup, ListGroupItem, Panel, Image, DropdownButton, Grid, Row, Col, Thumbnail, Button,FormGroup , ControlLabel, FormControl } from 'react-bootstrap'
 import '../App.css'
 import ManageProfile from './ManageProfile'
+import axios from 'axios'
 
 
 class TenentsPage extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        tenent: []
+      }
+    }
+
+  componentWillMount() {
+   //axios.get(`/api/users/${this.props.params.id}`)
+   axios.get('http://localhost:3000/api/tenents/2')
+      .then(response => {
+        this.setState({
+          tenent : response.data[0],
+        })
+      })
+      .catch(err => {
+      })
+  }
+
+
+
   render() {
 
     const profilebutton = (
@@ -28,7 +50,7 @@ class TenentsPage extends Component {
       <Row>
       <Col xs={12} sm={12}>
         <Thumbnail src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-1/p320x320/15940632_10211188445325227_1050933472522230573_n.jpg?oh=5126570961512041c40029a5615d1dae&oe=59CE6147" alt="260x260">
-          <h3>Tenent Profile</h3>
+          <h3>{this.state.tenent.first_name} {this.state.tenent.last_name}</h3>
           <p>{profileOverview}</p>
           <p>{profilebutton}</p>
         </Thumbnail>
