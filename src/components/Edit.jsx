36bff2410a10e8edf359d8ca7fcb5565b87290e7
Erica Rottman <erica7650@gmail.com>
2017-07-11 14:51:09 -0700

    working put route for boxes

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -11,6 +11,7 @@ class Edit extends Component {
     this.state = {
       box: {},
       values: {
+        id:'',
         identifier:'',
         unit_number: '',
         date_packed: '',
@@ -22,15 +23,22 @@ class Edit extends Component {
   }
   this.handleSubmit = this.handleSubmit.bind(this)
   this.handleSubmitPut = this.handleSubmitPut.bind(this)
+  this.handleIdChange = this.handleIdChange.bind(this)
+  this.handleIdentifierChange = this.handleIdentifierChange.bind(this)
   this.handleUnitChange = this.handleUnitChange.bind(this)
   this.handlePackedChange = this.handlePackedChange.bind(this)
   this.handleStoredChange = this.handleStoredChange.bind(this)
+  this.handleRoomChange = this.handleRoomChange.bind(this)
+  this.handleLocationChange = this.handleLocationChange.bind(this)
+  this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
 }
+
+
 
 
 handleSubmit(e) {
   e.preventDefault()
-  axios.get('http://localhost:3000/api/boxes/2')
+  axios.get('http://localhost:3000/api/boxes/1')
     .then(response => {
       console.log(response.data);
       this.setState({
@@ -41,32 +49,35 @@ handleSubmit(e) {
     })
   }
 
-  componentDidMount() {
-   //axios.get(`/api/users/${this.props.params.id}`)
-   axios.get('http://localhost:3000/api/boxes/2')
-      .then(response => {
-        console.log(response.data);
-        this.setState({
-          box : response.data,
-        })
-      })
-      .catch(err => {
-      })
-    }
-
 
   handleSubmitPut(e) {
     e.preventDefault()
-    axios.get('http://localhost:3000/api/boxes/2')
+    axios.put('http://localhost:3000/api/boxes/1', this.state.values)
       .then(response => {
         console.log(response.data);
         this.setState({
           box: response.data,
         })
       })
-      .catch(err => {
-      })
+  }
+
+
+    handleIdChange(e) {
+      console.log('string', e.target.value);
+      this.setState({values: {
+        id: e.target.value,
+        }
+       });
     }
+
+
+  handleIdentifierChange(e) {
+    console.log('string', e.target.value);
+    this.setState({values: {
+      identifier: e.target.value,
+      }
+     });
+  }
 
   handleUnitChange(e) {
     console.log('string', e.target.value);
@@ -92,6 +103,30 @@ handleSubmit(e) {
      });
   }
 
+  handleRoomChange(e) {
+    console.log('string', e.target.value);
+    this.setState({values: {
+      room: e.target.value,
+      }
+     });
+  }
+
+  handleLocationChange(e) {
+    console.log('string', e.target.value);
+    this.setState({values: {
+      unit_location: e.target.value,
+      }
+     });
+  }
+
+  handleDescriptionChange(e) {
+    console.log('string', e.target.value);
+    this.setState({values: {
+      description: e.target.value,
+      }
+     });
+  }
+
   render() {
     return (
       <div>
@@ -100,12 +135,12 @@ handleSubmit(e) {
             <FormGroup
               controlId="formBasicText"
               >
-              <ControlLabel>Box Identifier</ControlLabel>
+              <ControlLabel>Box Id</ControlLabel>
               <FormControl
                 type="text"
-                value={this.state.values.identifier}
+                value={this.state.values.id}
                 placeholder="example box2"
-                onChange={this.handleChange}
+                onChange={this.handleIdChange}
               />
               <FormControl.Feedback />
               <HelpBlock>Click Edit Button to Prefill Info </HelpBlock>
@@ -162,6 +197,7 @@ handleSubmit(e) {
       <FormGroup controlId="formControlsSelect">
          <ControlLabel>Room</ControlLabel>
          <FormControl
+           onChange={this.handleRoomChange}
            componentClass="select"
            placeholder={this.state.box.room}>
            <option value="select">{this.state.box.room}</option>
@@ -176,32 +212,36 @@ handleSubmit(e) {
 
        <FormGroup controlId="formControlsSelect">
           <ControlLabel>Unit Location</ControlLabel>
-          <FormControl componentClass="select" placeholder="select">
-            <option value="select">Select Location</option>
-            <option value="front_left">Front Left</option>
-            <option value="front_center">Front Center</option>
-            <option value="front_right">Front Right</option>
-            <option value="middle_left">Middle Left</option>
-            <option value="middle_center">Middle Center</option>
-            <option value="middle_right">Middle Right</option>
-            <option value="back_left">Back Left</option>
-            <option value="back_center">Back Center</option>
-            <option value="back_right">Back Right</option>
+          <FormControl
+            onChange={this.handleLocationChange}
+            componentClass="select"
+            placeholder="select">
+            <option value="select">{this.state.box.unit_location}</option>
+            <option value={this.state.values.unit_location}>Front Left</option>
+            <option value={this.state.values.unit_location}>Front Center</option>
+            <option value={this.state.values.unit_location}>Front Right</option>
+            <option value={this.state.values.unit_location}>Middle Left</option>
+            <option value={this.state.values.unit_location}>Middle Center</option>
+            <option value={this.state.values.unit_location}>Middle Right</option>
+            <option value={this.state.values.unit_location}>Back Left</option>
+            <option value={this.state.values.unit_location}>Back Center</option>
+            <option value={this.state.values.unit_location}>Back Right</option>
           </FormControl>
         </FormGroup>
 
 
        <FormGroup controlId="formControlsTextarea">
        <ControlLabel>Description</ControlLabel>
-       <FormControl componentClass="textarea"  />
+       <FormControl
+         componentClass="textarea"
+         value={this.state.values.description}
+         placeholder={this.state.box.description}
+         onChange={this.handleDescriptionChange}
+       />
      </FormGroup>
      <Button type="submit">
           Update Box
         </Button>
-        <Button type="submit">
-          Delete Box
-        </Button>
-
 </form>
 </div>
 </div>
