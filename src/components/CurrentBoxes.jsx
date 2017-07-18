9abd533c52a38ea3ab7d83c27e2da0092847485b
Erica Rottman <erica7650@gmail.com>
2017-07-18 10:06:30 -0700

    working sort function on boxes

--- src/components/CurrentBoxes.jsx
+++ src/components/CurrentBoxes.jsx
@@ -11,8 +11,10 @@ class CurrentBoxes extends Component {
     super(props)
     this.state = {
       boxes:[],
+      sortBy: 'name',
     }
     this.fetchBox = this.fetchBox.bind(this)
+    this.sortTable = this.sortTable.bind(this)
   }
 
   fetchBox(box) {
@@ -45,6 +47,24 @@ class CurrentBoxes extends Component {
     )
   }
 
+sortTable(column) {
+  let sortBoxes = this.state.boxes
+  // if(column === 'Date Stored') {
+    sortBoxes.sort(function (a, b) {
+      if(a[column] > b[column]) {
+        return 1
+      }
+      if(a[column] < b[column] ) {
+        return -1
+      }
+      return 0
+    })
+    this.setState({
+      boxes: sortBoxes
+    })
+  // }
+}
+
 
 
 render() {
@@ -52,9 +72,9 @@ render() {
     <Table striped bordered condensed hover>
       <thead>
         <tr >
-          <th>Name</th>
-          <th>Date Stored</th>
-          <th>Location</th>
+          <th onClick={() => this.sortTable('identifier')} >Name</th>
+          <th onClick={() => this.sortTable('date_stored')}>Date Stored</th>
+          <th onClick={() => this.sortTable('unit_location')}>Location</th>
         </tr>
       </thead>
       <tbody>
